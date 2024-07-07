ac810a52dbb3af0f5c516a3417355960843f0234
Arafa9785 <mohamadyasar.7863@gmail.com>
2024-07-07 14:01:42 +0530

    PremDone

--- src/test/java/org/sample/Sample.java
+++ src/test/java/org/sample/Sample.java
@@ -8,7 +8,12 @@ public class Sample {
 		System.out.println("Git Project Start");
 		String s = "New Project add to Git";
 		System.out.println(s);
-		
+		System.out.println("Prem Start");
+		String s4 = "Java Program";
+		System.out.println(s4);
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
 		
 	}
 }
