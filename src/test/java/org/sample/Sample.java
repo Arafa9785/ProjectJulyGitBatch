4e7745aaf0298a96821097856f6a093e914ebb2d
Arafa9785 <mohamadyasar.7863@gmail.com>
2024-07-07 13:00:00 +0530

    GitProjectDone

---
+++ src/test/java/org/sample/Sample.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Program 1");
+		System.out.println("Git Project Start");
+		String s = "New Project add to Git";
+		System.out.println(s);
+		
+		
+	}
+}
